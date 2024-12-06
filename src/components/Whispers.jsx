34be6b024c5144815a7c1617fb421f7090f728cb
Eduardo J. Morales <eduardojosegonzalezmorales41@gmail.com>
2024-12-06 20:10:17 +0000

    Already created the logic for the components whispers and also the logic that I'm goint to apply to each of the others components.

--- src/components/Whispers.jsx
+++ src/components/Whispers.jsx
@@ -1,14 +1,27 @@
 import { createSignal } from 'solid-js';
 
 function App() {
-  const [content, setContent] = createSignal('Contenido inicial');
+  const [content, setContent] = createSignal('');
+  const [Name, setName] = createSignal('');
 
+ const whispers = [
+    { name: 1, content: 'Whisper 1' },
+    { name: 2, content: 'Whisper 2' },
+    { name: 3, content: 'Whisper 3' },
+  ]
+
+  const handleContent = (id) => {
+    setContent(whispers.find(whisper => whisper.name === id)?.content);
+    setName(whispers.find(whisper => whisper.name === id)?.name);
+  }
   return (
     <div>
-      <div>
-        <button onClick={() => setContent('Contenido 1')}>Botón 1</button>
-        <button onClick={() => setContent('Contenido 2')}>Botón 2</button>
-        <button onClick={() => setContent('Contenido 3')}>Botón 3</button>
+        <div className="justify-center space-x-4 p-4 bg-gray-100">
+        {whispers.map(item => (
+          <button 
+          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-opacity-50"
+          onClick={() => handleContent(item.name)}>{item.name}</button>
+        ))}
       </div>
       <div id="contenido">
         {content()}
